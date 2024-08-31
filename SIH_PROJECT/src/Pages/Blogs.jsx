61cbb93e8b5a0cb0fda51fcf3a2b782569a63d25
Angad Sudan <angadsudan453@gmail.com>
2024-08-31 18:35:42 +0530

    updated the images and corrected few font mistakes

--- SIH_PROJECT/src/Pages/Blogs.jsx
+++ SIH_PROJECT/src/Pages/Blogs.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import {Card} from "../Components/index"
-import src from "../Assets/Logo.png"
+import src from "../Assets/Logo.jpg"
 import "../Assets/Styling/Blog.css"
 function Blogs() {
   return (
     <>
     <div>
-
       <h1 className='text-[44px] pl-2'>BLOGS</h1>
       <div className='bg-[#f0eeee] h-[90svh] w-full p-2'>
       <h2 className='mb-8'>A Directory of Wonderful Things</h2>
         <div className='flex justify-evenly '>
           <Card img={src} Title="A Prescription for Success" preview="Abbhas Khanna,postmouth alumnus and founder of click chemist,shares his advice on how to properlly invest time into your passion....." />
-          <Card img={src} Title="From ROCKBOTTOM,TO BRIGHTER DAYS" preview="A story of mental illnes from graduate,Vikas Verma,wants to share that hoe important mental heath is forhealty body and mind....."/>
+          <Card img={src} Title="From Rockbottom,To Brighter Days" preview="A story of mental illnes from graduate,Vikas Verma,wants to share that hoe important mental heath is forhealty body and mind....."/>
           <Card img={src} Title="A SNAPSHOT, OF MY JOURNEY"  preview="A walk down the memory lane, today I just felt like writing about my graduation journey and sharing a few memories with all of you..." />
         </div>
       </div>
